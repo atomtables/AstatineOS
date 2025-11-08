67ac15ae127cda7f38ed1279083f722c254745e6
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2025-11-08 11:48:53 -0500

    allow running without rebuild

--- build.sh
+++ build.sh
@@ -3,42 +3,6 @@
 # uses .build.log for saving previous hashes
 set -e
 
-if [ -e ".build.log" ]; then
-#   source .build.log
-    :
-else
-  echo "Running a clean compilation."
-fi
-
-# ───────────────────────────────────────────────
-# only rebuild if file changed
-# ───────────────────────────────────────────────
-
-file_changed() {
-    local SRC="$1"
-    local val1="${SRC//\//_}"
-    local val="${val1//./_}_last"
-    local current_checksum=$(cat "$SRC" | cksum | awk '{ printf $1 }')
-    
-    if [ "$current_checksum" = "${!val}" ]; then
-        return 1  # unchanged
-    fi
-    return 0  # changed
-}
-update_checksum() {
-    local SRC="$1"
-    local val1="${SRC//\//_}"
-    local val="${val1//./_}_last"
-    local current_checksum=$(cat "$SRC" | cksum | awk '{ printf $1 }')
-    
-    if [ "${!val}" = "" ]; then
-        echo -n "export $val=" >> .build.log
-        echo "$current_checksum" >> .build.log
-    else
-        sed -i '' "s/${!val}/${current_checksum}/g" .build.log
-    fi
-}
-
 # ───────────────────────────────────────────────
 # CONFIGURATION
 # ───────────────────────────────────────────────
@@ -119,139 +83,185 @@ update_checksum() {
     KERNEL_DIR_SRC_CHANGED=false
     BOOTSECTOR_SRC_CHANGED=false
     BOOTLOADER_SRC_CHANGED=false
-# ───────────────────────────────────────────────
-# COMPILE KERNEL (C + ASM)
-# ───────────────────────────────────────────────
 
-echo "[*] Compiling kernel sources..."
-find "${KERNEL_DIR}" -type f -name "*.c" | while read -r SRC; do
-    if ! file_changed "$SRC"; then
-        continue
-    fi
-    KERNEL_DIR_SRC_CHANGED=true
-    OBJ="${BUILD_DIR}/${SRC%.c}.o"
-    mkdir -p "$(dirname "$OBJ")"
-    echo " -> $SRC"
-    $CROSS_GCC -c "$SRC" -o "$OBJ" $CCFLAGS_KERNEL
-    update_checksum "$SRC"
+
+NO_BUILD=false
+for arg in "$@"; do
+  if [ $arg = "--no-build" ]; then
+      NO_BUILD=true
+  fi
 done
 
-find "${KERNEL_DIR}" -type f -name "*.asm" | while read -r SRC; do
-    if ! file_changed "$SRC"; then
-        continue
+if [ "$NO_BUILD" = false ]; then
+    if [ -e ".build.log" ]; then
+    #   source .build.log
+        :
+    else
+        echo "Running a clean compilation."
     fi
-    KERNEL_DIR_SRC_CHANGED=true
-    OBJ="${BUILD_DIR}/${SRC%.asm}.o"
-    mkdir -p "$(dirname "$OBJ")"
-    echo " -> $SRC"
-    $NASM $NASMFLAGS "$SRC" -o "$OBJ"
-    update_checksum "$SRC"
-done
 
-# Collect all object files
-KERNEL_OBJECTS=$(find "$BUILD_DIR/$KERNEL_DIR" -type f -name "*.o")
+    # ───────────────────────────────────────────────
+    # only rebuild if file changed
+    # ───────────────────────────────────────────────
 
-# ───────────────────────────────────────────────
-# LINK KERNEL
-# ───────────────────────────────────────────────
+    file_changed() {
+        local SRC="$1"
+        local val1="${SRC//\//_}"
+        local val="${val1//./_}_last"
+        local current_checksum=$(cat "$SRC" | cksum | awk '{ printf $1 }')
+        
+        if [ "$current_checksum" = "${!val}" ]; then
+            return 1  # unchanged
+        fi
+        return 0  # changed
+    }
+    update_checksum() {
+        local SRC="$1"
+        local val1="${SRC//\//_}"
+        local val="${val1//./_}_last"
+        local current_checksum=$(cat "$SRC" | cksum | awk '{ printf $1 }')
+        
+        if [ "${!val}" = "" ]; then
+            echo -n "export $val=" >> .build.log
+            echo "$current_checksum" >> .build.log
+        else
+            sed -i '' "s/${!val}/${current_checksum}/g" .build.log
+        fi
+    }
+    # ───────────────────────────────────────────────
+    # COMPILE KERNEL (C + ASM)
+    # ───────────────────────────────────────────────
 
-echo "[*] Linking kernel..."
-$LD -o "$KERNEL_IMG" $KERNEL_OBJECTS $LDFLAGS  
+    echo "[*] Compiling kernel sources..."
+    find "${KERNEL_DIR}" -type f -name "*.c" | while read -r SRC; do
+        if ! file_changed "$SRC"; then
+            continue
+        fi
+        KERNEL_DIR_SRC_CHANGED=true
+        OBJ="${BUILD_DIR}/${SRC%.c}.o"
+        mkdir -p "$(dirname "$OBJ")"
+        echo " -> $SRC"
+        $CROSS_GCC -c "$SRC" -o "$OBJ" $CCFLAGS_KERNEL
+        update_checksum "$SRC"
+    done
 
-# ───────────────────────────────────────────────
-# FORMAT FAT32 PARTITION
-# ───────────────────────────────────────────────
-echo "[*] Creating FAT32 partition..."
-if [ -e "$FAT32_PART_IMG" ]; then
-    rm "$FAT32_PART_IMG"
-fi
-$DD if=/dev/zero of="$FAT32_PART_IMG" bs=$FAT32_PART_SIZE count=1
-$MFORMAT -F -i "$FAT32_PART_IMG" ::
-$MCOPY -i "$FAT32_PART_IMG" "$KERNEL_IMG" ::
-$MCOPY -i "$FAT32_PART_IMG" $KERNEL_RESOURCES/* ::
+    find "${KERNEL_DIR}" -type f -name "*.asm" | while read -r SRC; do
+        if ! file_changed "$SRC"; then
+            continue
+        fi
+        KERNEL_DIR_SRC_CHANGED=true
+        OBJ="${BUILD_DIR}/${SRC%.asm}.o"
+        mkdir -p "$(dirname "$OBJ")"
+        echo " -> $SRC"
+        $NASM $NASMFLAGS "$SRC" -o "$OBJ"
+        update_checksum "$SRC"
+    done
 
-# ───────────────────────────────────────────────
-# ASSEMBLE BOOTSECTOR (make change in boot.asm to reflect)
-# ───────────────────────────────────────────────
-if file_changed "$BOOTSECT_SRC"; then
-    echo "[*] Assembling bootsector..."
-    $NASM -fbin "$BOOTSECT_SRC" -o "$BOOTSECT_BIN"
-    BOOTSECTOR_SRC_CHANGED=true
-    update_checksum "$BOOTSECT_SRC"
-fi
+    # Collect all object files
+    KERNEL_OBJECTS=$(find "$BUILD_DIR/$KERNEL_DIR" -type f -name "*.o")
 
-# ───────────────────────────────────────────────
-# ASSEMBLE BOOTLOADER (ABP)
-# ───────────────────────────────────────────────
+    # ───────────────────────────────────────────────
+    # LINK KERNEL
+    # ───────────────────────────────────────────────
 
-if file_changed "$BOOTLOADER_boot_aex_src"; then
-    echo "[*] Assembling bootloader..."
-    # compile the BOOT.AEX file
-    $NASM -fbin "$BOOTLOADER_boot_aex_src" -o "$BOOTLOADER_boot_aex"
-    BOOTLOADER_SRC_CHANGED=true
-    update_checksum "$BOOTLOADER_boot_aex_src"
-fi
+    echo "[*] Linking kernel..."
+    $LD -o "$KERNEL_IMG" $KERNEL_OBJECTS $LDFLAGS  
 
-# compile the LOADER.AEX file
-BOOTLOADER_OBJECTS=""
-# for each ASM file, compile it
-find "${BOOTLOADER_loader_aex_SRC}" -type f -name "*.asm" | while read -r SRC; do
-    OBJ="${BUILD_DIR}/${SRC%.asm}.o"
-
-    if ! file_changed "$SRC"; then
-        continue
+    # ───────────────────────────────────────────────
+    # FORMAT FAT32 PARTITION
+    # ───────────────────────────────────────────────
+    echo "[*] Creating FAT32 partition..."
+    if [ -e "$FAT32_PART_IMG" ]; then
+        rm "$FAT32_PART_IMG"
     fi
-    BOOTLOADER_SRC_CHANGED=true
+    $DD if=/dev/zero of="$FAT32_PART_IMG" bs=$FAT32_PART_SIZE count=1
+    $MFORMAT -F -i "$FAT32_PART_IMG" ::
+    $MCOPY -i "$FAT32_PART_IMG" "$KERNEL_IMG" ::
+    $MCOPY -i "$FAT32_PART_IMG" $KERNEL_RESOURCES/* ::
 
-    mkdir -p "$(dirname "$OBJ")"
-    echo " -> $SRC"
-    $NASM $NASMFLAGS "$SRC" -o "$OBJ"
-    update_checksum "$SRC"
-done
-# for each C file, compile it
-find "${BOOTLOADER_loader_aex_SRC}" -type f -name "*.c" | while read -r SRC; do
-    OBJ="${BUILD_DIR}/${SRC%.c}.o"
-
-    if ! file_changed "$SRC"; then
-        continue
+    # ───────────────────────────────────────────────
+    # ASSEMBLE BOOTSECTOR (make change in boot.asm to reflect)
+    # ───────────────────────────────────────────────
+    if file_changed "$BOOTSECT_SRC"; then
+        echo "[*] Assembling bootsector..."
+        $NASM -fbin "$BOOTSECT_SRC" -o "$BOOTSECT_BIN"
+        BOOTSECTOR_SRC_CHANGED=true
+        update_checksum "$BOOTSECT_SRC"
     fi
-    BOOTLOADER_SRC_CHANGED=true
 
-    mkdir -p "$(dirname "$OBJ")"
-    echo " -> $SRC"
-    $CROSS_GCC -c "$SRC" -o "$OBJ" $CCFLAGS_KERNEL
-    update_checksum "$SRC"
-done
-BOOTLOADER_loader_aex_OBJECTS=$(find "$BUILD_DIR/$BOOTLOADER_loader_aex_SRC" -type f -name "*.o")
-# link the bootloader
-if [ "$BOOTLOADER_SRC_CHANGED" = true ]; then
-    echo $BOOTLOADER_loader_aex_OBJECTS
-    $LD -o "$BOOTLOADER_loader_aex" $BOOTLOADER_loader_aex_OBJECTS -m elf_i386 -Ttext 0x8000 --oformat binary
-    $MKABP "$BOOTLOADER_COMP_DIR" "$BOOTLOADER_BIN"
-    echo
+    # ───────────────────────────────────────────────
+    # ASSEMBLE BOOTLOADER (ABP)
+    # ───────────────────────────────────────────────
+
+    if file_changed "$BOOTLOADER_boot_aex_src"; then
+        echo "[*] Assembling bootloader..."
+        # compile the BOOT.AEX file
+        $NASM -fbin "$BOOTLOADER_boot_aex_src" -o "$BOOTLOADER_boot_aex"
+        BOOTLOADER_SRC_CHANGED=true
+        update_checksum "$BOOTLOADER_boot_aex_src"
+    fi
+
+    # compile the LOADER.AEX file
+    BOOTLOADER_OBJECTS=""
+    # for each ASM file, compile it
+    find "${BOOTLOADER_loader_aex_SRC}" -type f -name "*.asm" | while read -r SRC; do
+        OBJ="${BUILD_DIR}/${SRC%.asm}.o"
+
+        if ! file_changed "$SRC"; then
+            continue
+        fi
+        BOOTLOADER_SRC_CHANGED=true
+
+        mkdir -p "$(dirname "$OBJ")"
+        echo " -> $SRC"
+        $NASM $NASMFLAGS "$SRC" -o "$OBJ"
+        update_checksum "$SRC"
+    done
+    # for each C file, compile it
+    find "${BOOTLOADER_loader_aex_SRC}" -type f -name "*.c" | while read -r SRC; do
+        OBJ="${BUILD_DIR}/${SRC%.c}.o"
+
+        if ! file_changed "$SRC"; then
+            continue
+        fi
+        BOOTLOADER_SRC_CHANGED=true
+
+        mkdir -p "$(dirname "$OBJ")"
+        echo " -> $SRC"
+        $CROSS_GCC -c "$SRC" -o "$OBJ" $CCFLAGS_KERNEL
+        update_checksum "$SRC"
+    done
+    BOOTLOADER_loader_aex_OBJECTS=$(find "$BUILD_DIR/$BOOTLOADER_loader_aex_SRC" -type f -name "*.o")
+    # link the bootloader
+    if [ "$BOOTLOADER_SRC_CHANGED" = true ]; then
+        echo $BOOTLOADER_loader_aex_OBJECTS
+        $LD -o "$BOOTLOADER_loader_aex" $BOOTLOADER_loader_aex_OBJECTS -m elf_i386 -Ttext 0x8000 --oformat binary
+        $MKABP "$BOOTLOADER_COMP_DIR" "$BOOTLOADER_BIN"
+        echo
+    fi
+
+    # ───────────────────────────────────────────────
+    # CREATE ISO IMAGE
+    # ───────────────────────────────────────────────
+
+    if [ "$KERNEL_DIR_SRC_CHANGED" = true ] || [ "$BOOTSECTOR_SRC_CHANGED" = true ] || [ "$BOOTLOADER_SRC_CHANGED" = true ]; then
+        echo "[*] Creating bootable ISO image..."
+        $DD if=/dev/zero of="$ISO_IMG" bs=512 count=1
+        $DD if="$BOOTSECT_BIN" of="$ISO_IMG" conv=notrunc bs=512 seek=0 count=1
+        $DD if="$BOOTLOADER_BIN" of="$ISO_IMG" conv=notrunc seek=1 bs=512
+        SEEK=$(($(($(wc -c < $ISO_IMG)))/512))
+        echo BOOTLOADER SIZE: $(($SEEK - 1))
+        $DD if="$FAT32_PART_IMG" of="$ISO_IMG" conv=notrunc bs=512 seek=$SEEK
+        # $DD if="$KERNEL_IMG" of="$ISO_IMG" conv=notrunc bs=512 seek=$SEEK
+
+        $MV "$ISO_IMG" "$FINAL_ISO"
+    fi
+
+    echo "[✔] ISO built at: $FINAL_ISO"
 fi
 
 # ───────────────────────────────────────────────
-# CREATE ISO IMAGE
-# ───────────────────────────────────────────────
-
-if [ "$KERNEL_DIR_SRC_CHANGED" = true ] || [ "$BOOTSECTOR_SRC_CHANGED" = true ] || [ "$BOOTLOADER_SRC_CHANGED" = true ]; then
-    echo "[*] Creating bootable ISO image..."
-    $DD if=/dev/zero of="$ISO_IMG" bs=512 count=1
-    $DD if="$BOOTSECT_BIN" of="$ISO_IMG" conv=notrunc bs=512 seek=0 count=1
-    $DD if="$BOOTLOADER_BIN" of="$ISO_IMG" conv=notrunc seek=1 bs=512
-    SEEK=$(($(($(wc -c < $ISO_IMG)))/512))
-    echo BOOTLOADER SIZE: $(($SEEK - 1))
-    $DD if="$FAT32_PART_IMG" of="$ISO_IMG" conv=notrunc bs=512 seek=$SEEK
-    # $DD if="$KERNEL_IMG" of="$ISO_IMG" conv=notrunc bs=512 seek=$SEEK
-
-    $MV "$ISO_IMG" "$FINAL_ISO"
-fi
-
-echo "[✔] ISO built at: $FINAL_ISO"
-
-# ───────────────────────────────────────────────
-# (OPTIONAL) RUN IN QEMU
+# RUN IN QEMU
 # ───────────────────────────────────────────────
 
 if [[ "$1" == "run" ]]; then
